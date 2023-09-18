fe4021122fb41ee8eec58e348597e21b69c1202a
kurumadaisuke <kuruma0909@gmail.com>
2023-09-18 16:06:22 +0900

    変数名の変更。if文をブロックスタイルに変更。

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -1,17 +1,21 @@
 import minimist from "minimist";
-var argv = minimist(process.argv.slice(2), {});
 
-const year = Boolean(argv.y) == true ? argv.y : new Date().getFullYear();
-const month = Boolean(argv.m) == true ? argv.m : new Date().getMonth() + 1;
+const argv = minimist(process.argv.slice(2));
+const year = Boolean(argv.y) === true ? argv.y : new Date().getFullYear();
+const month = Boolean(argv.m) === true ? argv.m : new Date().getMonth() + 1;
 
-let last_date = new Date(year, month, 0);
+const lastDate = new Date(year, month, 0);
 console.log(`      ${month}月 ${year}`);
 console.log("日 月 火 水 木 金 土");
 
-for (let i = 1; i <= last_date.getDate(); i++) {
-  let padded_i = String(i).padStart(2, " ");
-  let first_date = new Date(year, month - 1, i);
-  if (i === 1) process.stdout.write("   ".repeat(first_date.getDay()));
-  process.stdout.write(padded_i + " ");
-  if (first_date.getDay() === 6) process.stdout.write("\n");
+for (let countDays = 1; countDays <= lastDate.getDate(); countDays++) {
+  let paddDays = String(countDays).padStart(2, " ");
+  let today = new Date(year, month - 1, countDays);
+  if (countDays === 1) {
+    process.stdout.write("   ".repeat(today.getDay()));
+  }
+  process.stdout.write(paddDays + " ");
+  if (today.getDay() === 6) {
+    process.stdout.write("\n");
+  }
 }
