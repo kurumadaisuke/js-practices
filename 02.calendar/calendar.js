2ddf69a03251670e049c80a418c5f121c06a7953
kurumadaisuke <kuruma0909@gmail.com>
2023-09-18 19:07:30 +0900

    Boolean()の利用をしないコード

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -1,21 +1,23 @@
 import minimist from "minimist";
 
 const argv = minimist(process.argv.slice(2));
-const year = Boolean(argv.y) === true ? argv.y : new Date().getFullYear();
-const month = Boolean(argv.m) === true ? argv.m : new Date().getMonth() + 1;
+const today =
+  argv.y === undefined || argv.m === undefined ? new Date() : undefined;
+const year = argv.y === undefined ? today.getFullYear() : argv.y;
+const month = argv.m === undefined ? today.getMonth() + 1 : argv.m;
 
 const lastDate = new Date(year, month, 0);
 console.log(`      ${month}月 ${year}`);
 console.log("日 月 火 水 木 金 土");
 
 for (let countDays = 1; countDays <= lastDate.getDate(); countDays++) {
-  let paddDays = String(countDays).padStart(2, " ");
-  let today = new Date(year, month - 1, countDays);
+  let padDays = String(countDays).padStart(2, " ");
+  let days = new Date(year, month - 1, countDays);
   if (countDays === 1) {
-    process.stdout.write("   ".repeat(today.getDay()));
+    process.stdout.write("   ".repeat(days.getDay()));
   }
-  process.stdout.write(paddDays + " ");
-  if (today.getDay() === 6 || countDays === lastDate.getDate()) {
+  process.stdout.write(padDays + " ");
+  if (days.getDay() === 6 || countDays === lastDate.getDate()) {
     process.stdout.write("\n");
   }
 }
