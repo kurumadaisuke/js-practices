4d0c62d1887e6112d406f84f0817f7dbafc3f4e9
kurumadaisuke <kuruma0909@gmail.com>
2023-09-19 20:36:29 +0900

    const定義修正

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -1,10 +1,10 @@
 import minimist from "minimist";
 
-const argv = minimist(process.argv.slice(2)),
-  today = new Date(),
-  year = argv.y ?? today.getFullYear(),
-  month = argv.m ?? today.getMonth() + 1,
-  lastDate = new Date(year, month, 0);
+const argv = minimist(process.argv.slice(2));
+const today = new Date();
+const year = argv.y ?? today.getFullYear();
+const month = argv.m ?? today.getMonth() + 1;
+const lastDate = new Date(year, month, 0);
 
 console.log(`      ${month}月 ${year}`);
 console.log("日 月 火 水 木 金 土");
