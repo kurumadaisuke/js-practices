1f791f6769b2e86085b0c3c2dc83f3eb215b167b
kurumadaisuke <kuruma0909@gmail.com>
2023-09-21 20:25:33 +0900

    変数名変更

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -4,21 +4,21 @@ const argv = minimist(process.argv.slice(2));
 const today = new Date();
 const year = argv.y ?? today.getFullYear();
 const month = argv.m ?? today.getMonth() + 1;
-const lastDate = new Date(year, month, 0);
+const lastDay = new Date(year, month, 0);
 
 console.log(`      ${month}月 ${year}`);
 console.log("日 月 火 水 木 金 土");
 
-for (let countDay = 1; countDay <= lastDate.getDate(); countDay++) {
-  const padDay = String(countDay).padStart(2, " ");
-  const day = new Date(year, month - 1, countDay);
-  if (countDay === 1) {
+for (let dayCounter = 1; dayCounter <= lastDay.getDate(); dayCounter++) {
+  const formattedDayString = String(dayCounter).padStart(2, " ");
+  const day = new Date(year, month - 1, dayCounter);
+  if (dayCounter === 1) {
     process.stdout.write("   ".repeat(day.getDay()));
   }
 
-  process.stdout.write(padDay);
+  process.stdout.write(formattedDayString);
 
-  if (day.getDay() === 6 || countDay === lastDate.getDate()) {
+  if (day.getDay() === 6 || dayCounter === lastDay.getDate()) {
     process.stdout.write("\n");
   } else {
     process.stdout.write(" ");
