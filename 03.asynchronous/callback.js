e6c7d018c06ff8af4a733b5c92768c8f7c8be264
kurumadaisuke <kuruma0909@gmail.com>
2023-09-23 21:11:56 +0900

    callback一旦完成。細かい調整は後で行う。

--- 03.asynchronous/callback.js
+++ 03.asynchronous/callback.js
@@ -2,45 +2,63 @@ import sqlite3 from "sqlite3";
 const db = new sqlite3.Database("./test.db");
 
 function createTable(callback) {
-  db.run(
-    "CREATE TABLE members (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE NOT NULL)",
-    () => {
-      console.log("テーブルを作成");
-      callback();
-    }
-  );
+  const createTableSql =
+    "CREATE TABLE members (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE NOT NULL)";
+
+  db.run(createTableSql, () => {
+    console.log("テーブルを作成");
+    callback();
+  });
 }
 
 function insertData(callback) {
-  db.run(
-    "INSERT INTO members (title) VALUES (?)",
-    "初めてのJavaScript",
-    function () {
-      const lastID = this.lastID;
-      console.log(`データを挿入 => ID: ${lastID}`);
-      callback();
-    }
-  );
+  const insertDataSql = "INSERT INTO members (title) VALUES (?)";
+
+  db.run(insertDataSql, "初めてのJavaScript", function () {
+    const lastID = this.lastID;
+    console.log(`データを挿入 => ID: ${lastID}`);
+    callback();
+  });
 }
 
 function getData(callback) {
-  db.get("SELECT * FROM members", (err, row) => {
+  const getDataSql = "SELECT * FROM members";
+
+  db.get(getDataSql, (err, row) => {
     console.log(`id:「${row.id}」 title:「${row.title}」`);
     callback();
   });
 }
 
 function deleteTable() {
-  db.run("DROP TABLE members", () => {
+  const deleteTable = "DROP TABLE members";
+
+  db.run(deleteTable, () => {
     console.log("テーブルを削除");
   });
 }
 
-createTable(() => {
-  insertData(() => {
-    getData(() => {
-      deleteTable(() => {
-        db.close();
+function dbClose() {
+  db.close();
+}
+
+createTable((error) => {
+  if (error) {
+    console.log("エラーメッセージ:", error.message);
+  }
+  insertData((error) => {
+    if (error) {
+      console.log("エラーメッセージ:", error.message);
+    }
+    getData((error) => {
+      if (error) {
+        console.log("エラーメッセージ:", error.message);
+      }
+      deleteTable((error) => {
+        if (error) {
+          console.log("エラーメッセージ:", error.message);
+        }
+        dbClose();
       });
     });
   });
--- 03.asynchronous/callback_Error.js
+++ 03.asynchronous/callback_Error.js
@@ -2,62 +2,84 @@ import sqlite3 from "sqlite3";
 const db = new sqlite3.Database("./test.db");
 
 function createTable(callback) {
-  db.run(
-    "CREATE TABLE members (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE NOT NULL)",
-    (error) => {
-      if (error) {
-        console.error("テーブル作成エラー", error);
-        callback(error);
-      } else {
-        console.log("テーブルを作成");
-        callback();
-      }
+  const createTableSql =
+    "CREATE TABLE members (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE NOT NULL)";
+
+  db.run(createTableSql, (error) => {
+    if (error) {
+      console.log("テーブル作成エラー");
+      callback(error);
+    } else {
+      console.log("テーブルを作成");
+      callback();
     }
-  );
+  });
 }
 
 function insertData(callback) {
-  db.run(
-    "INSERT INTO members (username) VALUES (?)",
-    "くるまだいすけ",
-    function (error) {
-      if (error) {
-        console.error("テーブル作成エラー", error);
-        callback(error);
-      } else {
-        const lastID = this.lastID;
-        console.log(`データを挿入 => ID: ${lastID}`);
-        callback();
-      }
+  const insertDataSql = "INSERT INTO members (username) VALUES (?)";
+
+  db.run(insertDataSql, "くるまだいすけ", function (error) {
+    if (error) {
+      console.log("データを挿入エラー");
+      callback(error);
+    } else {
+      const lastID = this.lastID;
+      console.log(`データを挿入 => ID: ${lastID}`);
+      callback();
     }
-  );
+  });
 }
 
 function getData(callback) {
-  db.get("SELECT * FROM members", (error, row) => {
-    if (error){
-      console.error("データ取得エラー:", error)
-      callback(error)
+  const getDataSql = "SELECT * FROM username";
+
+  db.get(getDataSql, (error, row) => {
+    if (error) {
+      console.log("データ取得エラー");
+      callback(error);
+    } else {
+      console.log(`id:「${row.id}」 title:「${row.title}」`);
+      callback();
     }
-    console.log(`id:「${row.id}」 title:「${row.title}」`);
-    callback();
   });
 }
 
-function deleteTable() {
-  db.run("DROP TABLE members", () => {
-    console.log("テーブルを削除");
+function deleteTable(callback) {
+  const deleteTable = "DROP TABLE username";
+
+  db.run(deleteTable, (error) => {
+    if (error) {
+      console.log("データベース削除エラー");
+      callback(error);
+    } else {
+      console.log("テーブルを削除");
+      callback();
+    }
   });
 }
 
-createTable((err) => {
-  if (err) {
-    console.error("エラー:", err);
+function dbClose() {
+  db.close();
+}
+
+createTable((error) => {
+  if (error) {
+    console.log("エラーメッセージ:", error.message);
   }
-  insertData(() => {
-    getData(() => {
-      deleteTable(() => {
-        db.close();
+  insertData((error) => {
+    if (error) {
+      console.log("エラーメッセージ:", error.message);
+    }
+    getData((error) => {
+      if (error) {
+        console.log("エラーメッセージ:", error.message);
+      }
+      deleteTable((error) => {
+        if (error) {
+          console.log("エラーメッセージ:", error.message);
+        }
+        dbClose();
       });
     });
   });
