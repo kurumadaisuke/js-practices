5d0b3bc3b1ecb31b7054d4e267d5a8ace0428d2a
kurumadaisuke <kuruma0909@gmail.com>
2023-09-23 16:41:00 +0900

    もう一箇所変数定義の箇所変更

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -4,11 +4,11 @@ const argv = minimist(process.argv.slice(2));
 const today = new Date();
 const year = argv.y ?? today.getFullYear();
 const month = argv.m ?? today.getMonth() + 1;
-const lastDay = new Date(year, month, 0);
 
 console.log(`      ${month}月 ${year}`);
 console.log("日 月 火 水 木 金 土");
 
+const lastDay = new Date(year, month, 0);
 for (let dayCounter = 1; dayCounter <= lastDay.getDate(); dayCounter++) {
   const day = new Date(year, month - 1, dayCounter);
   if (day.getDate() === 1) {
