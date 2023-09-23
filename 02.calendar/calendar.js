3f8dbb75fbdff11d50ca3daadd7e2baf290ea271
kurumadaisuke <kuruma0909@gmail.com>
2023-09-23 16:34:57 +0900

    変数定義箇所変更。day.getDate()での条件分岐処理の変更

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -10,15 +10,15 @@ console.log(`      ${month}月 ${year}`);
 console.log("日 月 火 水 木 金 土");
 
 for (let dayCounter = 1; dayCounter <= lastDay.getDate(); dayCounter++) {
-  const formattedDayString = String(dayCounter).padStart(2, " ");
   const day = new Date(year, month - 1, dayCounter);
-  if (dayCounter === 1) {
+  if (day.getDate() === 1) {
     process.stdout.write("   ".repeat(day.getDay()));
   }
 
+  const formattedDayString = String(dayCounter).padStart(2, " ");
   process.stdout.write(formattedDayString);
 
-  if (day.getDay() === 6 || dayCounter === lastDay.getDate()) {
+  if (day.getDay() === 6 || day.getDate() === lastDay.getDate()) {
     process.stdout.write("\n");
   } else {
     process.stdout.write(" ");
