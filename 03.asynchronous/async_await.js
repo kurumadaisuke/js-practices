e3e7da3c1cf03e96e4584b97c77a2901cd1d20c4
kurumadaisuke <kuruma0909@gmail.com>
2023-09-24 20:46:58 +0900

    ログ残し

--- 03.asynchronous/async_await.js
+++ 03.asynchronous/async_await.js
@@ -1,16 +1,17 @@
 import * as async_functions from "./async_functions_module.js";
 
 const createTablesql =
-  "CREATE TABLE members (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE NOT NULL)";
-const insertDatasql = "INSERT INTO members (title) VALUES (?)";
-const getDatasql = "SELECT * FROM members";
-const deleteTablesql = "DROP TABLE members";
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE NOT NULL)";
+const insertDatasql = "INSERT INTO books (title) VALUES (?)";
+const getDatasql = "SELECT * FROM books";
+const deleteTablesql = "DROP TABLE books";
 
 const async_await_func = async function () {
   await async_functions.createTable(createTablesql);
   await async_functions.insertData(insertDatasql, "初めてのJavaScript");
   await async_functions.getData(getDatasql);
   await async_functions.deleteTable(deleteTablesql);
+  await async_functions.dbClose();
 };
 
 async_await_func();
--- 03.asynchronous/async_await_Error.js
+++ 03.asynchronous/async_await_Error.js
@@ -1,24 +1,21 @@
-import * as async_functions from "./async_functions.js";
+import * as async_functions from "./async_functions_module.js";
 
 const createTablesql =
-  "CREATE TABLE members (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE NOT NULL)";
-const insertDatasql = "INSERT INTO members (username) VALUES (?)";
-const getDatasql = "SELECT * FROM users";
-const deleteTablesql = "DROP TABLE members";
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE NOT NULL)";
+const insertDatasql = "INSERT INTO books (username) VALUES (?)";
+const getDatasql = "SELECT * FROM usernames";
+const deleteTablesql = "DROP TABLE usernames";
 
 const async_await_func = async function () {
   try {
     await async_functions.createTable(createTablesql);
-    await async_functions.insertData(insertDatasql, "くるまだいすけ");
-    await async_functions.getData(getDatasql);
-    await async_functions.deleteTable(deleteTablesql);
   } catch (error) {
     console.log(error.message);
   } finally {
-    await async_functions.createTable(createTablesql);
     await async_functions.insertData(insertDatasql, "くるまだいすけ");
     await async_functions.getData(getDatasql);
     await async_functions.deleteTable(deleteTablesql);
+    await async_functions.dbClose();
   }
 };
 
