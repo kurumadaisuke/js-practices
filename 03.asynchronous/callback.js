39830998da346d89a2ac0ac9792af79fae55fe9d
kurumadaisuke <kuruma0909@gmail.com>
2023-10-06 22:55:02 +0900

    callback.jsの無駄な関数定義を削除

--- 03.asynchronous/callback.js
+++ 03.asynchronous/callback.js
@@ -1,53 +1,20 @@
 import sqlite3 from "sqlite3";
 const db = new sqlite3.Database("./test.db");
 
-function createTable(callback) {
-  const createTableSql =
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE NOT NULL)";
-
-  db.run(createTableSql, () => {
+db.run(
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE NOT NULL)",
+  () => {
     console.log("テーブルを作成");
-    callback();
-  });
-}
-
-function insertData(callback) {
-  const insertDataSql = "INSERT INTO books (title) VALUES (?)";
-
-  db.run(insertDataSql, "初めてのJavaScript", function () {
-    const lastID = this.lastID;
-    console.log(`データを挿入 => ID: ${lastID}`);
-    callback();
-  });
-}
-
-function getData(callback) {
-  const getDataSql = "SELECT * FROM books";
-
-  db.get(getDataSql, (error, book) => {
-    console.log(`id:「${book.id}」 title:「${book.title}」`);
-    callback();
-  });
-}
-
-function deleteTable() {
-  const deleteTable = "DROP TABLE books";
-
-  db.run(deleteTable, () => {
-    console.log("テーブルを削除");
-  });
-}
-
-function dbClose() {
-  db.close();
-}
-
-createTable(() => {
-  insertData(() => {
-    getData(() => {
-      deleteTable(() => {
-        dbClose();
+    db.run("INSERT INTO books (title) VALUES (?)", "初めてのJavaScript", () => {
+      const lastID = db.lastID;
+      console.log(`データを挿入 => ID: ${lastID}`);
+      db.get("SELECT * FROM books", (error, book) => {
+        console.log(`id:「${book.id}」 title:「${book.title}」`);
+        db.run("DROP TABLE books", () => {
+          console.log("テーブルを削除");
+          db.close();
+        });
       });
     });
-  });
-});
+  }
+);
