ad577e5343bfd44118dcdad2319ec886775ece6d
kurumadaisuke <kuruma0909@gmail.com>
2023-10-07 13:47:26 +0900

    標準エラー出力へ修正。テーブル作成/削除時にはエラー発生しない処理に修正。

--- 03.asynchronous/async_functions_module.js
+++ 03.asynchronous/async_functions_module.js
@@ -3,15 +3,9 @@ import sqlite3 from "sqlite3";
 const db = new sqlite3.Database("./test.db");
 
 function createTable(createTablesql) {
-  return new Promise((resolve, reject) => {
-    db.run(createTablesql, (error) => {
-      if (error) {
-        console.log("テーブル作成エラー");
-        reject(error);
-      } else {
-        console.log("テーブルを作成");
-        resolve();
-      }
+  return new Promise((resolve) => {
+    db.run(createTablesql, () => {
+      resolve();
     });
   });
 }
@@ -20,12 +14,10 @@ function insertData(insertDatasql, data) {
   return new Promise((resolve, reject) => {
     db.run(insertDatasql, data, function (error) {
       if (error) {
-        console.log("データ挿入エラー");
         reject(error);
       } else {
         const lastID = this.lastID;
-        console.log(`データを挿入 => ID: ${lastID}`);
-        resolve();
+        resolve(lastID);
       }
     });
   });
@@ -35,26 +27,18 @@ function getData(getDatasql) {
   return new Promise((resolve, reject) => {
     db.get(getDatasql, (error, book) => {
       if (error) {
-        console.log("データ取得失敗");
         reject(error);
       } else {
-        console.log(`id:「${book.id}」 title:「${book.title}」`);
-        resolve();
+        resolve(book);
       }
     });
   });
 }
 
 function deleteTable(deleteTablesql) {
-  return new Promise((resolve, reject) => {
-    db.run(deleteTablesql, (error) => {
-      if (error) {
-        console.log("テーブル削除失敗");
-        reject(error);
-      } else {
-        console.log("テーブルを削除");
-        resolve();
-      }
+  return new Promise((resolve) => {
+    db.run(deleteTablesql, () => {
+      resolve();
     });
   });
 }
