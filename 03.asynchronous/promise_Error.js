ae3a41ae403d2ac9b295b77e8ab17c7af9450fda
kurumadaisuke <kuruma0909@gmail.com>
2023-10-07 22:25:52 +0900

    promise処理完成

---
+++ 03.asynchronous/promise_Error.js
@@ -0,0 +1,33 @@
+import * as async_functions from "./async_functions_module.js";
+
+const createTablesql =
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE NOT NULL)";
+const insertDatasql = "INSERT INTO books (username) VALUES (?)";
+const getDatasql = "SELECT * FROM usernames";
+const deleteTablesql = "DROP TABLE usernames";
+
+async_functions
+  .createTable(createTablesql)
+  .then(() => {
+    console.log("テーブルを作成");
+  })
+
+  .then(() => async_functions.insertData(insertDatasql, "くるまだいすけ"))
+  .then((lastID) => {
+    console.log(`データを挿入 => ID: ${lastID}`);
+  })
+  .catch((error) => console.error(error.message))
+
+  .then(() => async_functions.getData(getDatasql))
+  .then((book) => {
+    console.log(`id:「${book.id}」 title:「${book.title}」`);
+  })
+  .catch((error) => console.error(error.message))
+
+  .then(() => async_functions.deleteTable(deleteTablesql))
+  .then(() => {
+    console.log("テーブルを削除");
+  })
+  .catch((error) => console.error(error.message))
+
+  .then(() => async_functions.dbClose());
